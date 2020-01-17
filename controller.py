39c0747e6c796485ef887ad28f5204e79e7a65b2
gboned <gboned@cifpfbmoll.eu>
2020-01-17 16:29:09 +0100

    refactor: Importado Root a controller.py
    Importado Root al módulo controller, y quitado comentario de una de las
    opciones

--- controller.py
+++ controller.py
@@ -3,11 +3,12 @@ from flask_restful import Resource, Api
 
 from resources.inventario import Inventario
 from config import *
+from resources.root import Root
 
 app = Flask(__name__)
 api = Api(app)
 
-# api.add_resource(Root, '/')
+api.add_resource(Root, '/')
 api.add_resource(Inventario, '/inventario')
 # api.add_resource(UpdateQuality, '/updateQuality')
 
