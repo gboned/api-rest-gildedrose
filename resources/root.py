94327abc68ef149cb1dc655e26a1ec8d9b5db6c6
gboned <gboned@cifpfbmoll.eu>
2020-01-21 02:50:39 +0100

    refactor: Modificado root

--- resources/root.py
+++ resources/root.py
@@ -1,5 +1,8 @@
+from flask_restful import Resource, Api
+from config import tienda
+from services.service import Service
 
-class Root():
+class Root(Resource):
     def __init__(self, as_view):
         self.as_view = {}
     def saludo(self):
