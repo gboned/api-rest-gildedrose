06e7b1601079c0e46bd532df1cbc302e17e8ee5b
gboned <gboned@cifpfbmoll.eu>
2020-01-21 02:44:57 +0100

    feat: Añadida clase a updateQuality
    Añadida clase a updateQuality.py que actualice la calidad

---
+++ resources/updateQuality.py
@@ -0,0 +1,8 @@
+from flask_restful import Resource, Api
+from config import tienda
+from services.service import Service
+
+class UpdateQuality(Resource):
+
+    def get(self):
+        return Service.updateQuality(tienda)
