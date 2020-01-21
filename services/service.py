4468bb5d55450cfa04a6f4bf34d706cfc38678d6
gboned <gboned@cifpfbmoll.eu>
2020-01-21 02:41:59 +0100

    feat: Añadida función a Service
    Añadida función updateQuality a clase Service dentro de service.py

--- services/service.py
+++ services/service.py
@@ -8,3 +8,9 @@ class Service():
         for item in items:
             inventario[item.name] = item.__dict__
         return inventario
+
+    def updateQuality(tienda):
+        # Para actualizar la calidad de los items y devolver el estado
+        # actualizado de los mismos
+        tienda.update_quality()
+        return Service.getInventario(tienda)
