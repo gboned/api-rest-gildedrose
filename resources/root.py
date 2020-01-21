7b3fa44f315d1d7ee085d7abcfb6ae52e712ff88
gboned <gboned@cifpfbmoll.eu>
2020-01-21 02:58:42 +0100

    refactor: Refactorizado root.py

--- resources/root.py
+++ resources/root.py
@@ -3,10 +3,6 @@ from config import tienda
 from services.service import Service
 
 class Root(Resource):
-    def __init__(self, as_view):
-        self.as_view = {}
-    def saludo(self):
-        self.as_view['welcome!'] = 'Ollivanders'
-        return self.as_view
-x = Root('a').saludo()
-print(x)
+
+    def get(self):
+        return {'Welcome!': 'Ollivanders'}
